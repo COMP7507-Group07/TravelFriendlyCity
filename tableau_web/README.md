e79b917300e1693d00b15eb5aae9ccee1ca7c760
ZhouXJ <zxjsxs@qq.com>
2023-11-27 22:49:32 +0800

    web set

---
+++ tableau_web/README.md
@@ -0,0 +1,11 @@
+# Introduction
+This is the web application using for Visual7507 demo pre. I just put the UI framework into the file, in case you don't have node.js and npm. The code will not be updated, so if there is any change in UI framework source code, you can use:
+
+    npm i layui
+
+# Usage
+Using vscode extension Go-live is ok. Just make sure you are under the top dictionary tableau_web.
+
+# Some other words:(
+If you see nothing in web pages, this is probably because our result have expired in tableau public:(
+I don't know how long it will presist. So good luck!
---
+++ tableau_web/main.js
@@ -0,0 +1,40 @@
+tableau_view = document.getElementById('tableau-viz')
+//console.log(tableau_view)
+
+function jumpHome(){
+    tableau_view.src="https://public.tableau.com/views/Project_final_web_version/Final?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+}
+
+function jumpVacancy_rent_and_price(){
+    // tableau_view.src = "https://prod-uk-a.online.tableau.com/t/apysdh/views/Project_test_wylyh_v2_modify_vacancy_dashboard/sub_vacancy_rentprice"
+    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version/vacancy_sub_rentprice?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+}
+
+function jumpVacancy_vacancy(){
+    // tableau_view.src = "https://prod-uk-a.online.tableau.com/t/apysdh/views/Project_test_wylyh_v2_modify_vacancy_dashboard/sub_vacancy_vacancy"
+    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version/vacancy_sub_vacancy?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+}
+
+function jumpTransportation_intra(){
+    // tableau_view.src = "https://prod-uk-a.online.tableau.com/t/apysdh/views/Project_test_wyl_v1/Transportation_Intra"
+    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version/Transportation_Intra?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+}
+
+function jumpTransportation_inter(){
+    // tableau_view.src = "https://prod-uk-a.online.tableau.com/t/apysdh/views/Project_test_wyl_v1/Transportation_Inter"
+    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version/Transportation_Inter?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+}
+
+function jumpRetail_detailed(){
+    // tableau_view.src = "https://prod-uk-a.online.tableau.com/t/apysdh/views/Project_final_wyl_zh/retails_detailedtype"
+    tableau_view.src = "https://public.tableau.com/views/Project_final_wyl_zh/retails_detailedtype?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+}
+
+function jumpRetail_total(){
+    // tableau_view.src = "https://prod-uk-a.online.tableau.com/t/apysdh/views/Project_final_wyl_zh/retails"
+    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version/retails?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+}
+
+function jumpConclusion(){
+    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version/Conclusion?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+}
