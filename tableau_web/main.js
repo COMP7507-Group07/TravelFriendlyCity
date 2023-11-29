b97f49a518253b242908d2ab7f17872f424a9148
ZhouXJ <zxjsxs@qq.com>
2023-11-29 20:05:05 +0800

    source code change and final report conclusion

--- tableau_web/main.js
+++ tableau_web/main.js
@@ -7,32 +7,32 @@ function jumpHome(){
 
 function jumpVacancy_rent_and_price(){
     // tableau_view.src = "https://prod-uk-a.online.tableau.com/t/apysdh/views/Project_test_wylyh_v2_modify_vacancy_dashboard/sub_vacancy_rentprice"
-    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version/vacancy_sub_rentprice?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+    tableau_view.src = "https://public.tableau.com/views/Project_test_modify_vacancy_dashboard_only_17012474649230/sub_vacancy_rentprice?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
 }
 
 function jumpVacancy_vacancy(){
     // tableau_view.src = "https://prod-uk-a.online.tableau.com/t/apysdh/views/Project_test_wylyh_v2_modify_vacancy_dashboard/sub_vacancy_vacancy"
-    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version/vacancy_sub_vacancy?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+    tableau_view.src = "https://public.tableau.com/views/Project_test_modify_vacancy_dashboard_only/sub_vacancy_vacancy?:language=en-US&:display_count=n&:origin=viz_share_link"
 }
 
 function jumpTransportation_intra(){
     // tableau_view.src = "https://prod-uk-a.online.tableau.com/t/apysdh/views/Project_test_wyl_v1/Transportation_Intra"
-    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version/Transportation_Intra?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version_17012519688700/Transportation_Intra?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link    "
 }
 
 function jumpTransportation_inter(){
     // tableau_view.src = "https://prod-uk-a.online.tableau.com/t/apysdh/views/Project_test_wyl_v1/Transportation_Inter"
-    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version/Transportation_Inter?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version_17012519688700/Transportation_Inter?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
 }
 
 function jumpRetail_detailed(){
     // tableau_view.src = "https://prod-uk-a.online.tableau.com/t/apysdh/views/Project_final_wyl_zh/retails_detailedtype"
-    tableau_view.src = "https://public.tableau.com/views/Project_final_wyl_zh/retails_detailedtype?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
+    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version_17011507581470/retails_detailedtype?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
 }
 
 function jumpRetail_total(){
     // tableau_view.src = "https://prod-uk-a.online.tableau.com/t/apysdh/views/Project_final_wyl_zh/retails"
-    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version_17011507581470/retails?:language=zh-CN&publish=yes&:display_count=n&:origin=viz_share_link"
+    tableau_view.src = "https://public.tableau.com/views/Project_final_web_version_17011507581470/retails?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
 }
 
 function jumpConclusion(){
